f8227f5917a1957ad23e2ba099f5922ca1bafe0d
rucp <rucp33@gmail.com>
2022-04-16 14:51:01 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+      name: 'Ruan Carlos',
+      counter: 0
+    };
+
+  handlePClick = () => {
+    this.setState({ name: 'Jr'}); 
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({ counter: counter + 1}); 
+  }
+
+  render() {
+    const { name, counter} = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Este é o link
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
+// function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+// }
 
 export default App;
