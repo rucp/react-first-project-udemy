e95cd555df31f569cf3c595376c25a82438ae6a4
rucp <rucp33@gmail.com>
2022-04-23 16:36:03 -0300

    testes

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -29,8 +29,14 @@ describe('<Button />', () => {
     });
 
     it('should be enable when disabled is false', () => {
+        const fn = jest.fn();
         render(<Button text="Load more" disabled={false} />);
         const button = screen.getByRole('button', { name: /load more/i });
         expect(button).toBeEnabled();
+    });
+    it('should match snapshot', () => {
+    const fn = jest.fn();
+    const { container } = render(<Button text="Load more" disabled={false} />);        
+    expect(container.firstChild).toMatchSnapshot();
     });
 })
